8584def2e44a173d2675987fbf19fcce1df93e38
RajinNath04 <rajin.mi2@gmail.com>
2024-07-30 00:57:34 +0530

    feat added the transaction section

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -6,32 +6,68 @@ const Transactions = () => {
     <div>
       <div className="container p-4 transaction-container">
         <h3 className="transaction-account-number">Fund Transfer</h3>
-        <div className="m-3 transaction-account-number">
+        <div className="m-3 transaction-account-number transaction-account">
           Account Number: <b>1234567890</b>{" "}
         </div>
-        <div className="m-3">
-          <input
-            type="text"
-            className="form-control"
-            // value={filterValue}
-            // onChange={(e) => setFilterValue(e.target.value)}
-            placeholder="Enter Account Number"
-          />
+        <div className="m-3 transaction-input-container">
+          <div className="transaction-input mr-2">
+            <input
+              type="text"
+              className="form-control"
+              // value={filterValue}
+              // onChange={(e) => setFilterValue(e.target.value)}
+              placeholder="Enter Account Number"
+            />
+          </div>
+          <div className="transaction-input mr-2">
+            <input
+              type="text"
+              className="form-control"
+              // value={filterValue}
+              // onChange={(e) => setFilterValue(e.target.value)}
+              placeholder="Enter Amount"
+            />
+          </div>
         </div>
-        <div className="m-3">
-          <input
-            type="text"
-            className="form-control"
-            // value={filterValue}
-            // onChange={(e) => setFilterValue(e.target.value)}
-            placeholder="Enter Amount"
-          />
-        </div>
-        <div className="m-3">
-          <button>Send</button>
+
+        <div className="m-3 transaction-button-section">
+          <div className="transaction-button">
+            <button>Send</button>
+          </div>
         </div>
       </div>
-      <div className="container"></div>
+      <div className="container p-4 transaction-container mt-4">
+        <h3 className="transaction-account-number">Multi currency transfer</h3>
+        <div className="m-3 transaction-account-number transaction-account">
+          Account Number: <b>1234567890</b>{" "}
+        </div>
+        <div className="m-3 transaction-input-container">
+          <div className="transaction-input mr-2">
+            <input
+              type="text"
+              className="form-control"
+              // value={filterValue}
+              // onChange={(e) => setFilterValue(e.target.value)}
+              placeholder="Enter Account Number"
+            />
+          </div>
+          <div className="transaction-input mr-2">
+            <input
+              type="text"
+              className="form-control"
+              // value={filterValue}
+              // onChange={(e) => setFilterValue(e.target.value)}
+              placeholder="Enter Amount"
+            />
+          </div>
+        </div>
+
+        <div className="m-3 transaction-button-section">
+          <div className="transaction-button">
+            <button>Send</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
